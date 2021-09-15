d53c1ab6863217611ead7f0cde3f7a418a1ab47a
quchao <chao.qu@daoyitong.com>
2021-09-15 15:35:38 +0800

    Initial Commit

---
+++ LookingFriend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LookingFriend
+//
+//  Created by 曲超 on 2021/9/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
